bd36bcfd6ff842cf787bfa7c9103c650353c5151
gotooooo <k.goto.123@gmail.com>
2023-04-25 15:44:52 +0900

    update script

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ACTIONS_WORKFLOWS_DIR=.github/workflows
+FILE_NAME=fmt.yml
+
 mkdir -p $ACTIONS_WORKFLOWS_DIR
 
 if [ -n "$(ls $ACTIONS_WORKFLOWS_DIR)" ]; then
@@ -8,25 +10,5 @@ if [ -n "$(ls $ACTIONS_WORKFLOWS_DIR)" ]; then
   exit 1
 fi
 
-echo 'name: fmt
-on: push
-jobs:
-  terraform-apply:
-    name: terraform fmt
-    runs-on: ubuntu-latest
-    timeout-minutes: 5
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v3
-      - name: set env
-        id: set-env
-        run: |
-          terraform_version=`cat .terraform-version`
-          echo $terraform_version
-          echo "terraform_version=$terraform_version" >> $GITHUB_OUTPUT
-      - uses: hashicorp/setup-terraform@v2
-        with:
-          terraform_version: ${{ steps.set-env.outputs.terraform_version }}
-      - name: terraform fmt
-        run: |
-          terraform fmt -check -recursive -list=true -diff' > $ACTIONS_WORKFLOWS_DIR/fmt.yml
+curl -fsSL https://raw.githubusercontent.com/gotooooo/terraform-module-initializer/main/files/$FILE_NAME > $ACTIONS_WORKFLOWS_DIR/$FILE_NAME
+echo "created $ACTIONS_WORKFLOWS_DIR/$FILE_NAME"
