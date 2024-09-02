7fbe631f209a43651fdd65282f811c88d5f1302f
gotokazuki <gotokazuki@pm.me>
2024-09-02 15:45:05 +0900

    move

--- initialize.sh
+++ initialize.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotooooo/terraform-module-initializer/main/scripts/editorconfig.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotooooo/terraform-module-initializer/main/scripts/gitattributes.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotooooo/terraform-module-initializer/main/scripts/gitignore.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotooooo/terraform-module-initializer/main/scripts/terraform.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotooooo/terraform-module-initializer/main/scripts/actions.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/terraform-module-initializer/main/scripts/editorconfig.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/terraform-module-initializer/main/scripts/gitattributes.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/terraform-module-initializer/main/scripts/gitignore.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/terraform-module-initializer/main/scripts/terraform.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/terraform-module-initializer/main/scripts/actions.sh)"
